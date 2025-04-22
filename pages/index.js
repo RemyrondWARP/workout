9be619d341cada079a077515a0aa7ac79a2da4a1
RemyrondWARP <remy@wearereasonablepeople.com>
2025-04-22 22:23:58 +0200

    second commit
    Let's try again

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+
+export default function Home() {
+  return (
+    <div style={{ padding: 20 }}>
+      <h1>Workout App (placeholder)</h1>
+      <p>Deze versie toont nog geen functionaliteit, maar is klaar voor installatie als PWA.</p>
+    </div>
+  );
+}
